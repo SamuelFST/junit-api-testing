9335823779cab0cd6a616b86360f842799d25166
SamuelFST <guti.samuel2002@gmail.com>
2024-04-09 20:53:53 -0300

    create main class test

--- src/test/java/com/apitesting/application/ApiTestingApplicationTests.java
+++ src/test/java/com/apitesting/application/ApiTestingApplicationTests.java
@@ -7,7 +7,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 class ApiTestingApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void main() {
+		ApiTestingApplication.main(new String[] {});
 	}
 
 }
