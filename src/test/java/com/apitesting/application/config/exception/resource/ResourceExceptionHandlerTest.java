4c612ee3bc4be6a8fe2eaba458fa1d60d67282b4
SamuelFST <guti.samuel2002@gmail.com>
2024-04-21 12:11:00 -0300

    data integrity exception test created

--- src/test/java/com/apitesting/application/config/exception/resource/ResourceExceptionHandlerTest.java
+++ src/test/java/com/apitesting/application/config/exception/resource/ResourceExceptionHandlerTest.java
@@ -1,5 +1,6 @@
 package com.apitesting.application.config.exception.resource;
 
+import com.apitesting.application.config.exception.service.DataIntegrityViolationException;
 import com.apitesting.application.config.exception.service.ObjectNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -16,6 +17,8 @@ class ResourceExceptionHandlerTest {
 
     private static final String OBJECT_NOT_FOUND = "Object not found";
     private static final int NOT_FOUND = HttpStatus.NOT_FOUND.value();
+    private static final String E_MAIL_ALREADY_IN_USE = "E-mail already in use";
+    private static final int BAD_REQUEST = HttpStatus.BAD_REQUEST.value();
     @InjectMocks
     private ResourceExceptionHandler exceptionHandler;
 
@@ -41,5 +44,17 @@ class ResourceExceptionHandlerTest {
 
     @Test
     void dataIntegrityViolationException() {
+        ResponseEntity<StandardError> response = exceptionHandler.dataIntegrityViolationException(
+                new DataIntegrityViolationException(E_MAIL_ALREADY_IN_USE),
+                new MockHttpServletRequest()
+        );
+
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+        assertEquals(BAD_REQUEST, response.getStatusCode().value());
+        assertEquals(BAD_REQUEST, response.getBody().getStatus());
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(StandardError.class, response.getBody().getClass());
+        assertEquals(E_MAIL_ALREADY_IN_USE, response.getBody().getError());
     }
 }
